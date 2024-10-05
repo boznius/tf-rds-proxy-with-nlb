4d7e716364f598b1113ba18b165b60a98f9bf384
boznius <bbozhev@gmail.com>
2024-10-05 17:54:11 +0300

    cleanup

--- data.tf
+++
@@ -1,9 +0,0 @@
-# data.tf
-
-# Retrieve the IP addresses stored in SSM Parameter Store
-data "aws_ssm_parameter" "rds_proxy_ips" {
-  name = var.ssm_parameter_name
-
-  # Ensure that the SSM parameter is retrieved after the Lambda function is invoked
-  depends_on = [null_resource.invoke_lambda]
-}
